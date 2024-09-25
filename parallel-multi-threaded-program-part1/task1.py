2eb66ee3107e4273564b53cd92411d6f131f78a0
evgenii <vovk.evgenii.199811@gmail.com>
2024-09-25 15:45:19 +0300

    change parallel-multi-threaded-program-part1

--- parallel-multi-threaded-program-part1/task1.py
+++ parallel-multi-threaded-program-part1/task1.py
@@ -3,12 +3,12 @@ from threading import Thread
 import time
 
 class Task:
-    def __init__(self):
+    def __init__(self) -> None:
         self.random_numbers = list()
         self.average = 0
         self.summa = 0
 
-    def ran_num(self):
+    def ran_num(self) -> None:
         for _ in range(10):
             numb = randint(1, 10)
             self.random_numbers.append(numb)
@@ -16,11 +16,11 @@ class Task:
             time.sleep(1)
         print(f"Список - {self.random_numbers}")
 
-    def sum_list(self):
+    def sum_list(self) -> None:
         self.summa = sum(self.random_numbers)
         print(f"Cумму списка - {self.summa}")
 
-    def average_list(self):
+    def average_list(self) -> None:
         self.average = sum(self.random_numbers) / len(self.random_numbers)
         print(f"Cреднеарифметическое списка - {self.average}")
 
@@ -35,4 +35,3 @@ t1.join()
 
 t2.start()
 t3.start()
-
