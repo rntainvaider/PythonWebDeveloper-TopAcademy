6ec4169b9fbcd8ac462d8388469e7eb2d111eba0
evgenii <vovk.evgenii.199811@gmail.com>
2024-09-25 16:41:11 +0300

    change folder parallel

--- parallel-multi-threaded-program-part1/task2-1.py
+++ parallel-multi-threaded-program-part1/task2-1.py
@@ -16,25 +16,26 @@ class GenerateAFileWithNumbers:
         with open(name_file, 'w+') as file:
             file.writelines(f"{number}\n" for number in self.random_numbers)
 
-class PrimeNumbers:
-    def __init__(self) -> None:
-        self.prime_numb = list()
+# class PrimeNumbers:
+#     def __init__(self) -> None:
+#         self.prime_numb = list()
 
-    def read_file_with_numbers(self) -> None:
-        with open(name_file, 'r') as file:
-            f = file.read()
-            for number in f:
-                print(number)
+#     def read_file_with_numbers(self) -> None:
+#         with open(name_file, 'r') as file:
+#             f = file.read()
+#             for number in f:
+#                 print(number)
 
-    def get_prime_numbers(self):
-        ...
+#     def get_prime_numbers(self):
+#         ...
 
 generate_a_file_with_numbers = GenerateAFileWithNumbers()
 
 generate_a_file_with_numbers.generate_random_numbers()
+# generate_a_file_with_numbers.write_numbers_to_file(name_file)
 
-prime_numbers = PrimeNumbers()
-print(prime_numbers.read_file_with_numbers())
+# prime_numbers = PrimeNumbers()
+# print(prime_numbers.read_file_with_numbers())
 
 t1 = Thread(target=generate_a_file_with_numbers.write_numbers_to_file(name_file))
 
